dc4c2432bebc8a83021a57f3a266e44af97dcec1
GH <gwanghyunjeon@gmail.com>
2022-08-29 00:00:42 +0900

    새로운 페이지 만들어서 redux 변수를 이용해보았음

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
-import styled from "styled-components";
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { makeAction } from "./redux/action.js";
 import { createStore } from "redux";
 import { reducer } from "./redux/reducer.js";
-import { Contentbox } from "./components/content.js";
-
-const Main = styled.div`
-  margin: 0 auto;
-  width: 500px;
-`;
-
-const Header = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-const Body = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const PostArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /* margin: 0 auto; */
-  width: 50%;
-`;
-const Title = styled.input`
-  background-color: white;
-  color: black;
-  width: 100%;
-  margin-top: 15px;
-  margin-right: 25px;
-`;
-const Content = styled.textarea`
-  background-color: white;
-  color: black;
-  margin-top: 5px;
-  margin-right: 25px;
-  width: 100%;
-`;
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Main.js";
+import { PageOne } from "./page/PageOne.js";
+import { PageTwo } from "./page/PageTwo.js";
 
 function App() {
-  const dispatch = useDispatch();
-  const contentList = useSelector((state) => state);
-  const [inputTitle, setInputTitle] = useState("");
-  const [inputContent, setInputContent] = useState("");
-  // console.log(contentList);
-  // console.log(inputTitle, inputContent);
-  console.log(contentList);
   return (
-    <Main>
-      <Header>
-        <PostArea>
-          <Title onChange={(e) => setInputTitle(e.target.value)} value={inputTitle} />
-          <Content onChange={(e) => setInputContent(e.target.value)} value={inputContent} />
-        </PostArea>
-        <button
-          onClick={() => {
-            dispatch(makeAction("ADD_ITEM", { title: inputTitle, content: inputContent }));
-          }}
-        >
-          submit
-        </button>
-      </Header>
-      <Body>{contentList && contentList.map((ele, idx) => <Contentbox key={idx} data={ele} />)}</Body>
-    </Main>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/pageone" element={<PageOne />}></Route>
+      <Route path="/pagetwo" element={<PageTwo />}></Route>
+    </Routes>
   );
 }
 export const store = createStore(reducer);
